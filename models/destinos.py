fe7eb6e65b3941ceea8f633bbee60dc9c07c31f1
Ezequiel <qferro@gmail.com>
2023-12-15 06:40:44 -0300

    nada nuevo

--- models/destinos.py
+++ models/destinos.py
@@ -4,15 +4,18 @@ from typing import Optional
 class Destino(BaseModel):
     id: Optional[str] = None
     lugar: str
+    area : str
+    provincia : str
     detalles: Optional[str] = None
 
     # detalles = [
     #     {
     #     "fecha": "23-12-2023",
+    #     "hora " : "08:00 am"
     #     "guiaid" : "9797sa9df7as0f7as0",
     #     "empresa": "una empresa",
-
-    #     }
+    #     "descripcion" : "Breve descripción"
+    #     },
     # ]
 
     
